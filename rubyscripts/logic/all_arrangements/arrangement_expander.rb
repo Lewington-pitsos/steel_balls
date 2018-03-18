4ac530b0ea829f5e702df2a2d0527f87aed84f81
lewington <lewington@student.unimelb.edu.au>
2018-03-18 12:16:52 +1100

    arrangement_expander weights vlass variable passing tests

--- rubyscripts/logic/all_arrangements/arrangement_expander.rb
+++ rubyscripts/logic/all_arrangements/arrangement_expander.rb
@@ -1,3 +1,13 @@
 class ArrangementExpander
 
+  @@weights = :heavier
+
+  def self.weights
+    @@weights
+  end
+
+  def self.weights=(value)
+    @@weights = value
+  end
+
 end
--- rubyscripts/testing/arrangement_expander_test.rb
+++ rubyscripts/testing/arrangement_expander_test.rb
@@ -9,36 +9,40 @@ class ArrangementExpanderTest < Minitest::Test
     @arr_generator = ArrangementGenerator.new()
   end
 
+  def test_class_weights
+    assert_equal :heavier, ArrangementExpander.weights
+    ArrangementExpander.weights = :both
+    assert_equal :both, ArrangementExpander.weights
+  end
+
   def test_default_weights
     assert_equal :heavier, @arr_expander.weights
-    assert_equal :heavier ArrangementExpander.weights
     ArrangementExpander.weights = :both
     new_expander = ArrangementExpander.new()
-    assert_equal :both ArrangementExpander.weights
     assert_equal :both, new_expander.weights
   end
 
   def test_generates_correct_arrangements
     balls = @arr_generator.generate_balls(2)
     all_possible_arrangements = @arr_expander.expand(balls)
-    assert_equal 2 all_possible_arrangements.length
+    assert_equal 2, all_possible_arrangements.length
 
     balls = @arr_generator.generate_balls(4)
     all_possible_arrangements = @arr_expander.expand(balls)
-    assert_equal 4 all_possible_arrangements.length
+    assert_equal 4, all_possible_arrangements.length
 
     ArrangementExpander.weights = :both
     new_expander = ArrangementExpander.new()
     balls = @arr_generator.generate_balls(4)
     all_possible_arrangements = new_expander.expand(balls)
-    assert_equal 8 all_possible_arrangements.length
+    assert_equal 8, all_possible_arrangements.length
   end
 
   def test_takes_ball_markings_into_account
     balls = @gen.generate_balls(4)
     balls[0].mark = :possibly_lighter
     all_possible_arrangements = @arr_expander.expand(balls)
-    assert_equal 1 all_possible_arrangements.length
+    assert_equal 1, all_possible_arrangements.length
 
     ArrangementExpander.weights = :both
     new_expander = ArrangementExpander.new()
@@ -46,10 +50,11 @@ class ArrangementExpanderTest < Minitest::Test
     balls.each { |ball| ball.mark = :normal }
     balls[0].mark = :possibly_heavier
     all_possible_arrangements = new_expander.expand(balls)
-    assert_equal 1 all_possible_arrangements.length
+    assert_equal 1, all_possible_arrangements.length
   end
 
 
   def teardown
   end
+
 end
