f52903de1a8036102c52c0e75e0f7c8f6fa01fb1
lewington <lewington@student.unimelb.edu.au>
2018-03-18 12:09:38 +1100

    arrangeemnt expander test up

--- rubyscripts/testing/arrangement_expander_test.rb
+++ rubyscripts/testing/arrangement_expander_test.rb
@@ -9,19 +9,44 @@ class ArrangementExpanderTest < Minitest::Test
     @arr_generator = ArrangementGenerator.new()
   end
 
-  def test_deafult_setting
-    assert_equal 8, @gen.length
+  def test_default_weights
+    assert_equal :heavier, @arr_expander.weights
+    assert_equal :heavier ArrangementExpander.weights
+    ArrangementExpander.weights = :both
+    new_expander = ArrangementExpander.new()
+    assert_equal :both ArrangementExpander.weights
+    assert_equal :both, new_expander.weights
   end
 
-  def test_generates_default_balls
-    balls = @gen.generate_balls()
-    assert_equal 8, balls.length
-    assert balls[0].mark == :unknown
+  def test_generates_correct_arrangements
+    balls = @arr_generator.generate_balls(2)
+    all_possible_arrangements = @arr_expander.expand(balls)
+    assert_equal 2 all_possible_arrangements.length
+
+    balls = @arr_generator.generate_balls(4)
+    all_possible_arrangements = @arr_expander.expand(balls)
+    assert_equal 4 all_possible_arrangements.length
+
+    ArrangementExpander.weights = :both
+    new_expander = ArrangementExpander.new()
+    balls = @arr_generator.generate_balls(4)
+    all_possible_arrangements = new_expander.expand(balls)
+    assert_equal 8 all_possible_arrangements.length
   end
 
-  def test_generates_different_length_arrays
+  def test_takes_ball_markings_into_account
     balls = @gen.generate_balls(4)
-    assert_equal 4, balls.length
+    balls[0].mark = :possibly_lighter
+    all_possible_arrangements = @arr_expander.expand(balls)
+    assert_equal 1 all_possible_arrangements.length
+
+    ArrangementExpander.weights = :both
+    new_expander = ArrangementExpander.new()
+    balls = @arr_generator.generate_balls(4)
+    balls.each { |ball| ball.mark = :normal }
+    balls[0].mark = :possibly_heavier
+    all_possible_arrangements = new_expander.expand(balls)
+    assert_equal 1 all_possible_arrangements.length
   end
 
 
