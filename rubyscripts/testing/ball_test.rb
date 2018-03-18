6617a6fa35dcff6dde7be8b9133dd6e6a134db1d
lewington <lewington@student.unimelb.edu.au>
2018-03-18 11:10:39 +1100

    ball passes all tetss

--- rubyscripts/testing/ball_test.rb
+++ rubyscripts/testing/ball_test.rb
@@ -7,9 +7,31 @@ class BallTest < Minitest::Test
     @ball = Ball.new()
   end
 
-  def method_name
-
+  def test_default_mark_is_unknown
+    assert_equal :unknown, @ball.mark
   end
+
+  def test_mark_can_change
+    @ball.mark = :normal
+    assert_equal :normal, @ball.mark
+  end
+
+  def test_default_weight
+    assert_equal 1 , @ball.weight
+  end
+
+  def test_cannot_chnage_weight_directly
+    assert_raises 'Error' do
+      @ball.weight = 5
+    end
+  end
+
+  def test_can_change_weight_indirectly
+    assert_equal 1 , @ball.weight
+    @ball.make_heavier
+    assert_equal 2, @ball.weight
+  end
+
 
   def teardown
   end
