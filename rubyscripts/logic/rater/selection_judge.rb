1a437d7b74cc3b617873d0e5528deae143282960
lewington <lewington@student.unimelb.edu.au>
2018-03-18 23:02:58 +1100

    selection_judge commentry

--- rubyscripts/logic/rater/selection_judge.rb
+++ rubyscripts/logic/rater/selection_judge.rb
@@ -6,4 +6,12 @@ class SelectionJudge
 
   end
 
+  def score_selection(selection)
+    # ietrates throough all associated states and records the lowerst score among them. Sets the score of the current selection to that.
+    score = selection.map do |state|
+      state.score
+    end.min
+    selection[:score] = score
+  end
+
 end
