cec9d539943df4bb67552f0c18404769e3016ca8
lewington <lewington@student.unimelb.edu.au>
2018-03-18 20:51:19 +1100

    selection ceator scaffold

---
+++ rubyscripts/logic/selection_generator/all_selections_manager/selection_creator.rb
@@ -0,0 +1,37 @@
+
+class SelectionCreator
+
+  def initialize
+    @all_selections = []
+  end
+
+  def create_sates(split_permutations)
+
+  end
+
+  private
+
+  def create_selection(ball_array)
+
+  end
+
+  def default_selection
+    {
+      left: {
+        normal: 0,
+        not_lighter: 0,
+        not_heavier: 0,
+        unknown: 0,
+      },
+
+      right: {
+        normal: 0,
+        not_lighter: 0,
+        not_heavier: 0,
+        unknown: 0,
+      }
+    }
+  end
+
+
+end
