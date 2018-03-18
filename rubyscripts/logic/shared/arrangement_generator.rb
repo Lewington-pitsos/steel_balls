28e2cad86506bc035521a8172d1c8c1bd9957abd
lewington <lewington@student.unimelb.edu.au>
2018-03-18 17:38:31 +1100

    arrangement_generator length getting

--- rubyscripts/logic/shared/arrangement_generator.rb
+++ rubyscripts/logic/shared/arrangement_generator.rb
@@ -1,10 +1,10 @@
 # takes in a state and returns an array of balls that match the state
 
 require_relative './arrangement_generator/ball_generator'
-require_relative './arrangement_generator/ball_changer'
+require_relative './arrangement_generator/mark_changer'
 
 
-class ArangementGenerator
+class ArrangementGenerator
 
   @@default_length = 8
 
@@ -17,8 +17,13 @@ class ArangementGenerator
   def marked_balls(state)
     # generates a new array of blank balls, alters them to match the passed in state and returns them
     balls = @ball_generator.generate_balls()
-    @changer.state = state
-    @changer.marked_balls(balls)
+    @changer.marked_balls(balls, state)
+  end
+
+  private
+
+  def get_length(state)
+    state.inject(0) {|sum, counter|  sum + counter[1] }
   end
 
 end
--- rubyscripts/testing/arrangement_generator_test.rb
+++ rubyscripts/testing/arrangement_generator_test.rb
@@ -3,8 +3,48 @@ require_relative '../logic/shared/arrangement_generator'
 
 class ArrangementGeneratorTest < Minitest::Test
 
+  @@sample_state = {
+    unknown: 4,
+    possibly_heavier: 2,
+    possibly_lighter: 2,
+    normal: 0
+  }
+
+  @@sample_state2 = {
+    unknown: 2,
+    possibly_heavier: 2,
+    possibly_lighter: 2,
+    normal: 8
+  }
+
+  @@sample_state3 = {
+    unknown: 0,
+    possibly_heavier: 0,
+    possibly_lighter: 0,
+    normal: 0
+  }
+
   def setup
-    @gen = ArrangementGenerator.new()
+    @arrangement_generator = ArrangementGenerator.new()
+  end
+
+  def test_works_out_state_length
+    length = @arrangement_generator.send(:get_length, @@sample_state)
+    assert_equal 8, length
+
+    length = @arrangement_generator.send(:get_length, @@sample_state2)
+    assert_equal 14, length
+
+    length = @arrangement_generator.send(:get_length, @@sample_state3)
+    assert_equal 0, length
+  end
+
+  def test_generated_arrangments_match_state
+    balls = @arrangement_generator.marked_balls(@@sample_state)
+
+    @@sample_state.each do |mark, num|
+      assert_equal num, balls.count { |ball| ball.mark == mark }
+    end
   end
 
   def teardown
