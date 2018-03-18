2622ccf351f5e9d78855ebb608b62ab85a36bdf4
lewington <lewington@student.unimelb.edu.au>
2018-03-18 20:22:26 +1100

    state_permuter works with combinations

--- rubyscripts/testing/selection_generator/state_permuter_test.rb
+++ rubyscripts/testing/selection_generator/state_permuter_test.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require_relative '../../logic/selection_generator/all_selections_manager/state_permuter'
 
 class StatePermuterTest < Minitest::Test
-  
+
   @@unknown_state = {
     unknown: 8,
     possibly_lighter: 0,
@@ -17,21 +17,51 @@ class StatePermuterTest < Minitest::Test
     normal: 0
   }
 
+  @@small_state = {
+    unknown: 2,
+    possibly_lighter: 1,
+    possibly_heavier: 1,
+    normal: 0
+  }
+
+  @@array_permutations = '[[1, 2], [1, 3], [1, 4], [2, 1], [2, 3], [2, 4], [3, 1], [3, 2], [3, 4], [4, 1], [4, 2], [4, 3], [1, 2, 3, 4], [1, 2, 4, 3], [1, 3, 2, 4], [1, 3, 4, 2], [1, 4, 2, 3], [1, 4, 3, 2], [2, 1, 3, 4], [2, 1, 4, 3], [2, 3, 1, 4], [2, 3, 4, 1], [2, 4, 1, 3], [2, 4, 3, 1], [3, 1, 2, 4], [3, 1, 4, 2], [3, 2, 1, 4], [3, 2, 4, 1], [3, 4, 1, 2], [3, 4, 2, 1], [4, 1, 2, 3], [4, 1, 3, 2], [4, 2, 1, 3], [4, 2, 3, 1], [4, 3, 1, 2], [4, 3, 2, 1]]'
+
+
+  @@split_permutations = "[[[1], [2]], [[1], [3]], [[1], [4]], [[2], [1]], [[2], [3]], [[2], [4]], [[3], [1]], [[3], [2]], [[3], [4]], [[4], [1]], [[4], [2]], [[4], [3]], [[1, 2], [3, 4]], [[1, 2], [4, 3]], [[1, 3], [2, 4]], [[1, 3], [4, 2]], [[1, 4], [2, 3]], [[1, 4], [3, 2]], [[2, 1], [3, 4]], [[2, 1], [4, 3]], [[2, 3], [1, 4]], [[2, 3], [4, 1]], [[2, 4], [1, 3]], [[2, 4], [3, 1]], [[3, 1], [2, 4]], [[3, 1], [4, 2]], [[3, 2], [1, 4]], [[3, 2], [4, 1]], [[3, 4], [1, 2]], [[3, 4], [2, 1]], [[4, 1], [2, 3]], [[4, 1], [3, 2]], [[4, 2], [1, 3]], [[4, 2], [3, 1]], [[4, 3], [1, 2]], [[4, 3], [2, 1]]]"
+
   def setup
     @state_permuter = StatePermuter.new()
   end
 
   def test_generates_arrangement
-    @state_permuter.send(:generate_arrangement, @@unknown_state)
-    assert_equal 8, @state_permuter.send(:balls).length
+    balls = @state_permuter.send(:generate_arrangement, @@unknown_state)
+    assert_equal 8, balls.length
 
 
-    @state_permuter.send(:generate_arrangement, @@light_state)
-    assert_equal 12, @state_permuter.send(:balls).length
+    balls = @state_permuter.send(:generate_arrangement, @@light_state)
+    assert_equal 12, balls.length
     assert_equal 4,
-      @state_permuter.send(:balls).count { |b| b.mark == :unknown }
+      balls.count { |b| b.mark == :unknown }
     assert_equal 8,
-      @state_permuter.send(:balls).count { |b| b.mark == :possibly_lighter }
+      balls.count { |b| b.mark == :possibly_lighter }
+  end
+
+  def test_permutes_even_lengthed_arrays_properly
+    array = [1, 2, 3, 4]
+    @state_permuter.send(:generate_all_permutations, array)
+    assert_equal @@array_permutations, @state_permuter.send(:perms).to_s
+  end
+
+  def test_splits_permutations_in_half
+    array = [1, 2, 3, 4]
+    @state_permuter.send(:generate_all_permutations, array)
+    @state_permuter.send(:split_permutations)
+    assert_equal @@split_permutations, @state_permuter.send(:perms).to_s
+  end
+
+  def test_returns_list_of_split_permutations
+    perms = @state_permuter.send(:permute_state, @@small_state)
+    assert_equal 36, perms.length
   end
 
   def teardown
