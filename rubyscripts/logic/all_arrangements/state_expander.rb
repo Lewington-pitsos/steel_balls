d439c7005c20e29db7d26aea4f8cdfeccdc50a57
lewington <lewington@student.unimelb.edu.au>
2018-03-18 18:05:37 +1100

    state_exapnder commentry

--- rubyscripts/logic/all_arrangements/state_expander.rb
+++ rubyscripts/logic/all_arrangements/state_expander.rb
@@ -1,3 +1,5 @@
+# gets passed in a state and from that state, generates all possible arrays of balls (i.e. the possible different positions of differently weighted balls) and returns them
+
 require_relative '../shared/arrangement_generator'
 
 class StateExpander
