10d21c577fe54af1a35f5f484f09f4a9f5dde05d
lewington <lewington@student.unimelb.edu.au>
2018-03-18 22:56:13 +1100

    state_judge passing basic tests

--- rubyscripts/logic/rater/state_judge.rb
+++ rubyscripts/logic/rater/state_judge.rb
@@ -1,3 +1,38 @@
+# gets passed in a weight_outcome object, which should map a whole bunch of selectors to the ball states which result from making weighs according to that selector.
+
+# passes out nothing. It modifies the weigh_outcome by scoring every state.
+
 class StateJudge
+
+  @@scores = {
+    unknown: 0,
+    possibly_lighter: 2,
+    possibly_heavier: 2,
+    normal: 5
+  }
+
+  def initialize
+
+  end
+
+  def judge_states(weigh_outcome)
+    weigh_outcome.each do |selection|
+      score_states(selection)
+    end
+  end
+
+  private
+
+  def score_states(selection)
+    selection.each do |state|
+      state[:score] = score(state)
+    end
+  end
+
+  def score(state)
+    state.inject(0) do |total, counter|
+      total += counter[1] * @@scores[counter[0]]
+    end
+  end
 
 end
--- rubyscripts/testing/rater/state_judge_test.rb
+++ rubyscripts/testing/rater/state_judge_test.rb
@@ -3,8 +3,27 @@ require_relative '../../logic/rater/state_judge'
 
 class StateJudgeTest < Minitest::Test
 
+  @@light_state = {
+    unknown: 3,
+    possibly_heavier: 0,
+    possibly_lighter: 1,
+    normal: 0
+  }
+
+  @@normal_state = {
+    unknown: 0,
+    possibly_heavier: 0,
+    possibly_lighter: 1,
+    normal: 3
+  }
+
   def setup
-    @ball = StateJudge.new()
+    @judge = StateJudge.new()
+  end
+
+  def test_gets_correct_scores
+    assert_equal 2, @judge.send(:score, @@light_state)
+    assert_equal 17, @judge.send(:score, @@normal_state)
   end
 
   def teardown
