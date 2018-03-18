9b3d216d3c02ebe91528071ec0da1f8e40966409
lewington <lewington@student.unimelb.edu.au>
2018-03-18 14:03:15 +1100

    arrangement generator test scaffold up

---
+++ rubyscripts/logic/shared/arrangement_generator.rb
@@ -0,0 +1,7 @@
+# takes in a state and returns an array of balls that match the state
+
+class ArangementGenerator
+
+  
+
+end
---
+++ rubyscripts/testing/ball_generator_test.rb
@@ -0,0 +1,29 @@
+require "minitest/autorun"
+require_relative '../logic/shared/ball'
+require_relative '../logic/shared/ball_generator'
+
+class BallGeneratorTest < Minitest::Test
+
+  def setup
+    @gen = BallGenerator.new()
+  end
+
+  def test_deafult_length
+    assert_equal 8, @gen.length
+  end
+
+  def test_generates_default_balls
+    balls = @gen.generate_balls()
+    assert_equal 8, balls.length
+    assert balls[0].mark == :unknown
+  end
+
+  def test_generates_different_length_arrays
+    balls = @gen.generate_balls(4)
+    assert_equal 4, balls.length
+  end
+
+
+  def teardown
+  end
+end
