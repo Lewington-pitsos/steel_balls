a519cfe3e05b4cfd902695dae58aa320ff308a00
lewington <lewington@student.unimelb.edu.au>
2018-03-21 22:35:54 +1100

    scale prototype up

--- rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_manager/weigh_collector/scale_manager/scale.rb
+++ rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_manager/weigh_collector/scale_manager/scale.rb
@@ -11,6 +11,7 @@
 require_relative './scale/balancer'
 require_relative './scale/weigh_executer'
 require_relative '../../../../../../../../shared/ball_helper'
+require_relative '../../../../../../../../shared/state_generator'
 
 class Scale
 
@@ -21,7 +22,9 @@ class Scale
   def initialize
     @balancer = Balancer.new()
     @executor = WeighExecutor.new()
+    @state_generator = StateGenerator.new()
     @arrangements = []
+    @generated_state = {}
   end
 
   def weigh(selection_order)
@@ -33,10 +36,21 @@ class Scale
   def generate_all_balance_states
     # for each selection, triggers a balance of that selection with ALL of the passed in ball arrangements
     @selection_order[:balls].each do |arrangement|
-      balls = duplicate_balls(arrangement)
-      @balancer.balance(@selection_order, balls)
-      balance_state = @balancer.balance_state
-      arrangement = @executor.weigh(balance_state)
+      apply_conversions(arrangement)
+      @selection_order[:balances] << @generated_state
     end
+  end
+
+  def apply_conversions(arrangement)
+    # first we create a clone of the ball arrangement
+    # then we weigh the selection against that cloned ball arrangement and create a balance state
+    # then we convert that into an altered arragmenet based on weigh results
+    # then we convert that arrangeemnt into a normal ball state and save it
+    balls = duplicate_balls(arrangement)
+    @balancer.balance(@selection_order, balls)
+    balance_state = @balancer.balance_state
+    
+    arrangement = @executor.weigh(balance_state)
+    @generated_state = @state_generator.generate_state(state)
   end
 end
