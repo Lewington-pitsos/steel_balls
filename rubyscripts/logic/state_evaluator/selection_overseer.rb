abf8516fef2b3d92f1ae356cb9866b19ddf10085
lewington <lewington@student.unimelb.edu.au>
2018-03-21 14:33:52 +1100

    selection overseer gathers arrangements correctly

--- rubyscripts/logic/state_evaluator/selection_overseer.rb
+++ rubyscripts/logic/state_evaluator/selection_overseer.rb
@@ -1,11 +1,13 @@
 # takes in a ball state and holds a captive Omni selector and a captive State expander. It gets he Omni selector to generate all possible selection orders and the state expander to generate all possible ball arrangements. it then maps each selection order to a ball arrangement, and returns this huge agglomoration.
 
-require_relative './selector/omni_selector'
-require_relative './all_arrangements/state_expander'
-require_relative '../../shared/ball_helper'
+require_relative './selection_overseer/selector/omni_selector'
+require_relative './selection_overseer/all_arrangements/state_expander'
+require_relative '../shared/ball_helper'
 class SelectionOverseer
 
   include BallHelper
+
+  attr_accessor :weigh_orders
 
   def initialize(state)
     @state = state
@@ -17,6 +19,7 @@ class SelectionOverseer
   end
 
   def selections_to_weigh
+    # gathers all the selection orders and arrangements. Merges them by adding a copy of each arrangement to each selection order, and then returns all the seletion orders
     get_all_selection_orders
     get_all_arranments
     merge
@@ -24,6 +27,8 @@ class SelectionOverseer
   end
 
   private
+
+  attr_accessor :arrangements, :selection_orders
 
   def get_all_selection_orders
     @omni_selector.get_all_possible_selections(@state)
--- rubyscripts/testing/state_evaluator/selection_overseer_test.rb
+++ rubyscripts/testing/state_evaluator/selection_overseer_test.rb
@@ -1,10 +1,45 @@
 require "minitest/autorun"
-
+require_relative '../../logic/state_evaluator/selection_overseer'
 
 class SelectionOverseerTest < Minitest::Test
 
+  @@small_state = {
+    unknown: 2,
+    possibly_heavier: 0,
+    possibly_lighter: 0,
+    normal: 0
+  }
+
+  @@medium_state = {
+    unknown: 4,
+    possibly_heavier: 0,
+    possibly_lighter: 0,
+    normal: 0
+  }
+
+  @@light_state = {
+    unknown: 3,
+    possibly_heavier: 0,
+    possibly_lighter: 1,
+    normal: 0
+  }
+
+
   def setup
-    @overseer = SelectionOverseer.new()
+    @overseer = SelectionOverseer.new(@@small_state)
+  end
+
+  def test_gathers_arrangements_correctly
+    @overseer.send(:get_all_arranments)
+    assert_equal 2, @overseer.send(:arrangements).length
+
+    overseer = SelectionOverseer.new(@@medium_state)
+    overseer.send(:get_all_arranments)
+    assert_equal 4, overseer.send(:arrangements).length
+
+    overseer = SelectionOverseer.new(@@light_state)
+    overseer.send(:get_all_arranments)
+    assert_equal 3, overseer.send(:arrangements).length
   end
 
   def teardown
