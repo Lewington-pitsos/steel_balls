38a2dac8ead901975702a63963f20ed5aef9f7e9
lewington <lewington@student.unimelb.edu.au>
2018-03-21 15:24:48 +1100

    file system shakeup

--- rubyscripts/testing/rater/selection_judge_test.rb
+++ rubyscripts/testing/rater/selection_judge_test.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require_relative '../../logic/state_evaluator/score_overseer/rater/selection_judge'
+require_relative '../../logic/state_evaluator/score_overseer/rater/rater_manager/judgement_overseer/selection_judge'
 
 class SelectionJudgeTest < Minitest::Test
 
