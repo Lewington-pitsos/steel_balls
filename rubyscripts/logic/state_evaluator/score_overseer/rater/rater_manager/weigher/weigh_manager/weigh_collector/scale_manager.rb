39e73f807d03b3fc018f77e8e28c16248df724d2
lewington <lewington@student.unimelb.edu.au>
2018-03-21 23:20:25 +1100

    scale_manager passing first test battery

--- rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_manager/weigh_collector/scale_manager.rb
+++ rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_manager/weigh_collector/scale_manager.rb
@@ -11,12 +11,20 @@ class ScaleManager
   end
 
   def weigh(selection_order)
-    @left = selecion_order[:left]
-    @right = selection_order[:right]
-    @arrangements = selecion_order[:balls]
+    parse_selection_order(selection_order)
+    generate_all_selections
+    weigh_all_selections
   end
 
   private
+
+  attr_reader :selections
+
+  def parse_selection_order(selection_order)
+    @left = selection_order[:left]
+    @right = selection_order[:right]
+    @arrangements = selection_order[:balls]
+  end
 
   def generate_all_selections
     # for each selection in the list of possible selections for the right side of the weigh, we add a new simple selection object (also including the single selection for the left side) to the list of all simple selections
@@ -28,8 +36,8 @@ class ScaleManager
   def weigh_all_selections
     # for each selection generated we ask the scale to weigh that selection, passing in the original arrangements object for weighing each time
     @scale.arrangements = @arrangements
-    @selections.each do |selection|
-      @scale.weigh(selection)
+    @selections.each do |selection_order|
+      @scale.weigh(selection_order)
     end
   end
 
--- rubyscripts/testing/rater/scale_related/scale_manager_test.rb
+++ rubyscripts/testing/rater/scale_related/scale_manager_test.rb
@@ -1,20 +1,48 @@
 require "minitest/autorun"
 require_relative '../../../logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_manager/weigh_collector/scale_manager'
-require_relative '../../../logic/shared/arrangement_generator/ball_generator'
+require_relative '../../../logic/state_evaluator/selection_overseer/all_arrangements/state_expander'
 
 class ScaleManagerTest < Minitest::Test
 
-  @@normal_balls = BallGenerator.new.generate_balls
-
-  @@weird_balls = BallGenerator
-    .new
-    .generate_balls
-    .each { |ball| ball.make_heavier  }
-
   def setup
-    @lighter_balls = BallGenerator.new.generate_balls
-    @lighter_balls[0].make_lighter
-    @comparer = ScaleManager.new()
+    @normal_state = {
+      unknown: 8,
+      possibly_heavier: 0,
+      possibly_lighter: 0,
+      normal: 0
+    }
+
+    @normal_arrangements = StateExpander.new.expand(@normal_state)
+
+    @normal_selection = {
+      left: {
+        unknown: 2,
+        possibly_heavier: 0,
+        possibly_lighter: 0,
+        normal: 0
+      },
+      right: {
+        unknown: 2,
+        possibly_heavier: 0,
+        possibly_lighter: 0,
+        normal: 0
+      },
+      balls: @normal_arrangements
+    }
+
+    @normal_simple_selections = [{:left=>{:unknown=>2, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>0}, :right=>[:unknown, 2]}, {:left=>{:unknown=>2, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>0}, :right=>[:possibly_heavier, 0]}, {:left=>{:unknown=>2, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>0}, :right=>[:possibly_lighter, 0]}, {:left=>{:unknown=>2, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>0}, :right=>[:normal, 0]}]
+
+
+    @manager = ScaleManager.new()
+  end
+
+  def test_generates_selections_correctly
+    @manager.send(:parse_selection_order, @normal_selection)
+    @manager.send(:generate_all_selections)
+    assert_equal 4, @manager.send(:selections).length
+
+    assert_equal @normal_simple_selections.to_s, @manager.send(:selections).to_s
+
   end
 
   def teardown
