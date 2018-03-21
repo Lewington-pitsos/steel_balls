0d9fc0618aafbc24a84fe6a1b62a0a82f9483e04
lewington <lewington@student.unimelb.edu.au>
2018-03-21 16:12:44 +1100

    balancer prototype

--- rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_manager/weigh_collector/scale_manager/scale/balancer.rb
+++ rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_manager/weigh_collector/scale_manager/scale/balancer.rb
@@ -11,7 +11,7 @@
 class Balancer
 
   def initialize
-
+    @comparer = Comparer.new
   end
 
   def balance(selection_order, balls)
@@ -23,6 +23,7 @@ class Balancer
   end
 
   def categorize_balls
+    # splits the passed in ball array into a hash of arrays, each containing all and only the balls of a given mark
     @categories = Hash.new { |h, k| h[k] = [] }
     # a hash with different arrays as default values
 
@@ -32,17 +33,42 @@ class Balancer
   end
 
   def gather_balls(order)
+    # for each mark in the passed in order (a ball state) we take that many balls of that mark out of the categories hash and put them into an array
+    # we then return the array
     to_weigh = []
 
     order.each do |mark, number|
       number.times do
-        to_weigh << @categories[mark]
+        to_weigh << @categories[mark].pop()
       end
     end
+
+    to_weigh
   end
 
   def balance
+    @comparer.balance(@left, @right)
+    balance_state = default_balance_state
 
+    balance_state[:balanced] = comparer.balanced
+    balance_state[:heavier] = comparer.heavier
+    balance_state[:lighter] = comparer.lighter
+    balance_state[:unweighed] = agglomorate(@categories)
+
+  end
+
+  def default_balance_state
+    {
+      unweighed: [],
+      lighter: [],
+      heavier: [],
+      balanced: []
+    }
+  end
+
+  def agglomorate(category_hash)
+    # returns an vversion of the array hash compounded into a single array
+    category_hash.map { |_, array| array }.flatten
   end
 
 end
