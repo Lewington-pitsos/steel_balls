56d4bea9ed6b3fbaf07287850d0a1ad73dbb499d
lewington <lewington@student.unimelb.edu.au>
2018-03-22 12:42:33 +1100

    judge_manager prototype up

---
+++ rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/judgement_overseer/judge_manager.rb
@@ -0,0 +1,21 @@
+
+# gets passed in a weigh result (an array of selections mapped to states) and a minimum rating.
+# rates all the states, and selections, and then returns a new, rated weigh result with all the selections with ratigns lower than the mimimum removed.
+
+require_relative './judge_manager/selection_kuller'
+require_relative './judge_manager/judge'
+
+class JudgeManager
+
+  def initialize(minimum)
+    @kuller = SelectionKuller.new(minimum)
+    @judge = Judge.new()
+    @rated_weigh_result = []
+  end
+
+  def score_and_kull(weigh_result)
+    @judge.rate_selections(weigh_result)
+    @rated_weigh_result = @kuller.kulled(weigh_result)
+    @rated_weigh_result
+  end
+end
