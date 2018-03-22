2dd4dd35548139e4a3867821c1d5ba5c28cd05e5
lewington <lewington@student.unimelb.edu.au>
2018-03-22 12:28:28 +1100

    BadSelectionKiller prototype

--- rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/judgement_overseer/judge_manager/bad_state_killer.rb
+++ rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/judgement_overseer/judge_manager/bad_state_killer.rb
@@ -1,3 +1,16 @@
-class BadStateKiller
+# gets passed in a minimum rating and an array of scored selections.
+# It returns a new array that copies the passed in one exactly except that any selection whose score is equal to or lower (i think lower is impossible though) than the passed in rating is stricken from the new array.
+
+class BadSelectionKiller
+
+  def initialize(minimum_rating)
+    @min = minimum_rating
+  end
+
+  def kulled(seletions)
+    selections.select do |selection|
+      selection.rating > @min
+    end
+  end
 
 end
