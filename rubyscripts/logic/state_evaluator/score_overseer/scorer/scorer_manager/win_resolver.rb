50995ca10b7c3f0568d1d3a476549cfe76da32fb
lewington <lewington@student.unimelb.edu.au>
2018-03-22 14:00:20 +1100

    win_resolver passes all tests

---
+++ rubyscripts/logic/state_evaluator/score_overseer/scorer/scorer_manager/win_resolver.rb
@@ -0,0 +1,12 @@
+# gets passed in an array of scored_selections, e.g. :
+# {selection: {left: ball_state, right: ball_state }, score: int}
+
+# returns the score for the state (the lowest selection score + 1)
+
+class WinResolver
+
+  def state_score(scored_selections)
+    scored_selections.map { |sel| sel[:score] }.min + 1
+  end
+
+end
