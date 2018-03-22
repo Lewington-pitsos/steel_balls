dfcc2468abdb28b68f256707bc190469b8dc9819
lewington <lewington@student.unimelb.edu.au>
2018-03-22 13:39:53 +1100

    win_checker prototype up

---
+++ rubyscripts/logic/state_evaluator/score_overseer/scorer/scorer_manager/win_checker.rb
@@ -0,0 +1,34 @@
+# takes in an array of rated selections
+
+# checks all those weighted selections to see if any of them are winning selections (i.e. their rating is equal to (or somehow above) the winning rating)
+
+# if there are any, it gives them all a score of 0 and returns them all in an array
+
+# otherwise it returns an empty array
+
+class WinChecker
+
+  @@winning_rating = 37 # 7 normals and one posibly_heavier or possibly_lighter
+
+  def initialize
+    @winning_selections = []
+  end
+
+  def winners(rated_selections)
+    # goes through every selection. Is winning, all infromation regarding it's states and rating is deleted and it's added to the winning selections list with a score of 0
+    rated_selections.each do |selection|
+      if winner?(selection)
+        selection[:selection].delete(:states)
+        @@winning_selections << { selection: selection[:selection], score: 0 }
+      end
+    end
+
+    @winning_selections
+  end
+
+  private
+
+  def winner?(selection)
+    selection[:rating] >= @@winning_rating
+  end
+end
