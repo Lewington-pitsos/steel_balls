5b359306cac576b3e19e442bdf2341b222ef4bcb
lewington <lewington@student.unimelb.edu.au>
2018-03-22 14:14:33 +1100

    scorer_manager prototype

---
+++ rubyscripts/logic/state_evaluator/score_overseer/scorer/scorer_manager.rb
@@ -0,0 +1,37 @@
+# gets passed in an array of rated selections mapped to rated states
+
+# first it checks if any selectors are winners.
+
+# => If there are any winners, these are all scored at 0, and used to make an array of scored selection objects. The score for the sate is set to 1.
+
+# => otherwise the array of rated selections is passed to the selection manager, which eventually passes back a scored selection hash for each rated selection passed in. In this case the state score is set to the lowest scored selection + 1.
+
+# returns the score for the current state and an array of scored selection hashes
+
+require_relative './scorer_manager/win_resolver'
+require_relative './scorer_manager/win_checker'
+
+class ScorerManager
+
+  def intialize
+    @checker = WinChecker.new()
+    @resolver = WinResolver.new()
+  end
+
+  def score(rated_selections)
+    scored_selections = @checker.winners(rated_selections)
+
+    if !scored_selections.any?
+      score_selections(rated_selections)
+    end
+
+    state_score = @resolver.state_score(scored_selections)
+    {selections: scored_selections, state_score: state_score}
+  end
+
+  def score_selections
+    # to be added
+    false
+  end
+
+end
---
+++ rubyscripts/testing/scorer/scorer_manager_test.rb
@@ -0,0 +1,30 @@
+require "minitest/autorun"
+require_relative '../../logic/state_evaluator/score_overseer/scorer/scorer_manager/win_resolver'
+
+class WinResolverTest < Minitest::Test
+
+  @@eaxmple_winners = [
+    {selection: {}, score: 0},
+    {selection: {}, score: 0}
+  ]
+
+  @@eaxmple_winners2 = [
+    {selection: {}, score: 3},
+    {selection: {}, score: 2},
+    {selection: {}, score: 3},
+    {selection: {}, score: 3},
+    {selection: {}, score: 4}
+  ]
+
+  def setup
+    @resolver = WinResolver.new()
+  end
+
+  def test_returns_correct_state_score
+    assert_equal 1, @resolver.state_score(@@eaxmple_winners)
+    assert_equal 3, @resolver.state_score(@@eaxmple_winners2)
+  end
+
+  def teardown
+  end
+end
