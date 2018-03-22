2f8d56698088edf67c9a9d89b8270d2154e46d35
lewington <lewington@student.unimelb.edu.au>
2018-03-22 15:02:56 +1100

    selection_scorer prototype

---
+++ rubyscripts/logic/state_evaluator/score_overseer/scorer/scorer_manager/selection_manager/selection_scorer.rb
@@ -0,0 +1,32 @@
+# takes in an array of(rated) states
+# [ { rating: int, state: ball_state }, ... ]
+# returns the score for the associated selection
+
+# for each state, it creates a new state manager, passing in a single state (and it's rating). the state manager will pass back the SCORE for that state
+
+# all the states are listed out in an array, and the selection score becomes the same as the highest state score in that array (i.e. the worst possible outcome from weighing with the selection in question)
+
+# finally we return this score
+
+class SelectionScorer
+
+  def initialize
+    @state_scores = []
+  end
+
+  def score_selection(states)
+    states.each do |rated_state|
+      manager = StateManager.new(rated_state)
+      @state_scores << manager.score
+    end
+
+    highest_score
+  end
+
+  private
+
+  def highest_score
+    @state_scores.max
+  end
+
+end
