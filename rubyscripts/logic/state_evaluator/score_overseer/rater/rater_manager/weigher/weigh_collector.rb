2f72d7dd0bf360024d9bc86a92a54658585ca6f2
lewington <lewington@student.unimelb.edu.au>
2018-03-22 11:05:26 +1100

    weigh_collector prototype

---
+++ rubyscripts/logic/state_evaluator/score_overseer/rater/rater_manager/weigher/weigh_collector.rb
@@ -0,0 +1,28 @@
+# this guy gets passed in a whole huge bunch of complex selection orders
+# (these are hashes composed of:
+# => a single right selectioin
+# => an array of left selections
+# => an array of possible ball arrangements
+
+# for each order passed in, the WeighCollector gets the scale manager to generate a bunch of simple selections and weigh each against the passed in ball arrangement.
+
+# The scale_manager should pass back these simple selections, each mapped to all the UNIQUE ball states resulting from weighing each selection against that selection
+
+# All of these are then gathered into a big array of weighed selections
+
+require_relative './weigh_collector/scale_manager'
+
+class WeighCollector
+  def initialize
+    @scale_manager = ScaleManager.new()
+    @weighed_selections = []
+  end
+
+  def collect_all_weighs(all_selection_orders)
+    @weighed_selections = []
+    all_selection_orders.each do |selection_order|
+      @scale_manager.weigh(selection_order)
+      @weighed_selections += @scale_manager.selections
+    end
+  end
+end
