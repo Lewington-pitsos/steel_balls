0b754fcb7cfcbd212748f17506ab2c9e116ecab2
lewington <lewington@student.unimelb.edu.au>
2018-03-24 11:14:19 +1100

    state recorder revamp

--- rubyscripts/logic/database/state_recorder.rb
+++ rubyscripts/logic/database/state_recorder.rb
@@ -7,16 +7,38 @@ class StateRecorder < Archivist
 
   def initialize(name=@@database_name)
     super(name)
-    @selection_recorder = SelectionRecorder.new(name)
+    @ids = []
   end
 
-  def record_state(scored_state)
-    # selections = scored_state[:selections]
-    # @selection_recorder.save_selections(selections)
-    save_state(scored_state)
+  def record_state(all_states)
+    all_states.each do |state|
+      record_state_and_id(state)
+    end
   end
 
   private
+
+  def record_state_and_id(state)
+    id = get_state_id(state)
+    if !id
+      save_state(state)
+      id = most_recent_id()
+    end
+    @ids << id
+  end
+
+  def get_state_id(state)
+    @db.exec(
+      <<~COMMAND
+          SELECT id FROM scored_states
+            WHERE unknown = #{state[:unknown]} AND
+              possibly_lighter = #{state[:possibly_lighter]} AND
+              possibly_heavier = #{state[:possibly_heavier]} AND
+              normal = #{state[:normal]};
+          );
+      COMMAND
+    )
+  end
 
   def save_state(state)
     @db.exec(
@@ -26,15 +48,21 @@ class StateRecorder < Archivist
               possibly_lighter,
               possibly_heavier,
               normal,
-              score
             )
           VALUES (
             #{state[:state][:unknown]},
             #{state[:state][:possibly_lighter]},
             #{state[:state][:possibly_heavier]},
-            #{state[:state][:normal]},
-            #{state[:state_score]}
+            #{state[:state][:normal]}
           );
+      COMMAND
+    )
+  end
+
+  def most_recent_id
+    @db.exec(
+      <<~COMMAND
+          IDENT_CURRENT('scored_states');
       COMMAND
     )
   end
