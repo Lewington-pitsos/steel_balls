c9eec17ba85049c17ccb037cdfe81568a4fda416
lewington <lewington@student.unimelb.edu.au>
2018-03-24 22:21:55 +1100

    lookup prototype up

--- rubyscripts/logic/database/lookup.rb
+++ rubyscripts/logic/database/lookup.rb
@@ -8,60 +8,61 @@ class Lookup < Archivist
   @@optimal_tab = 'optimal_selections'
   @@resulting_tab = 'resulting_states'
 
+  attr_accessor :tree
+
   def initialize(name=@@database_name)
     super(name)
     @tree = {}
-    @current = nil
   end
 
   def build_tree
-    @tree = state_by_values(first_state)
+    first_id = state_id_by_values(first_state)
+    @tree = build_state(first_id)
   end
 
-  def state_by_values(state)
-    @db.exec(
-      <<~CMD
-        SELECT * FROM scored_states
-        unknown = #{state[:unknown]} AND
-          possibly_lighter = #{state[:possibly_lighter]} AND
-          possibly_heavier = #{state[:possibly_heavier]} AND
-          normal = #{state[:normal]};
-      CMD
-    )[0]
-  end
-
-  def build_selections(id)
-    selections = get_by_id(id, @@selection_tab)
-    selections.each do |selection|
-      selection['right'] = get_by_id(selection['right_id'], @@side_tab)
-      selection['left'] = get_by_id(selection['left_id'], @@side_tab)
+  def build_state(state_id)
+    state = get_by_id(state_id, @@state_tab)
+    selection_ids = optimal_selection_ids(state_id)
+    selections = selection_ids.map do |selection_id|
+      build_selection(selection_id)
     end
-    selections
-  end
-
-  def get_by_id(id, relation)
-    @db.exec(
-      <<~CMD
-        SELECT * FROM #{relation}
-        WHERE id = #{id};
-      CMD
-    )
-  end
-
-  def build_state_selection(state_id)
-    state = get_by_id(state_id, @@state_tab)[0]
-    selection_id = optimal_selection_id(state['id'])
-    selections = build_selections(selection_id)
     state['selections'] = selections
+    state
   end
 
-  def optimal_selection_id(state_id)
+  def build_selection(id)
+    selection = get_by_id(id, @@selection_tab)
+    selection['right'] = get_by_id(selection['right_id'], @@side_tab)
+    selection['left'] = get_by_id(selection['left_id'], @@side_tab)
+    selection['states'] = build_resulting_states(id)
+    selection
+  end
+
+  def build_resulting_states(selection_id)
+    state_ids = get_resulting_states(selection_id)
+    states = state_ids.map do |state_id|
+      build_state(state_id)
+    end
+  end
+
+  def first_state
+    {
+      unknown: $DEFAULT_LENGTH,
+      possibly_heavier: 0,
+      possibly_lighter: 0,
+      normal: 0
+    }
+  end
+
+  # ================ Query Methods ================
+
+  def optimal_selection_ids(state_id)
     @db.exec(
       <<~CMD
         SELECT selection_id FROM #{@@optimal_tab}
         WHERE state_id = #{state_id};
       CMD
-    )[0]['selection_id']
+    )
   end
 
   def get_resulting_states(selection_id)
@@ -73,20 +74,25 @@ class Lookup < Archivist
     )
   end
 
-  def build_resulting_states(selection_id)
-    state_ids = get_resulting_states(selection_id)
-    states = state_ids.map do |state_id|
-      build_state_selection(state_id)
-    end
+  def get_by_id(id, relation)
+    @db.exec(
+      <<~CMD
+        SELECT * FROM #{relation}
+        WHERE id = #{id};
+      CMD
+    )[0]
   end
 
-  def first_state
-    {
-      unknown: $DEFAULT_LENGTH,
-      possibly_heavier: 0,
-      possibly_lighter: 0,
-      normal: 0
-    }
+  def state_id_by_values(state)
+    @db.exec(
+      <<~CMD
+        SELECT * FROM scored_states
+        unknown = #{state[:unknown]} AND
+          possibly_lighter = #{state[:possibly_lighter]} AND
+          possibly_heavier = #{state[:possibly_heavier]} AND
+          normal = #{state[:normal]};
+      CMD
+    )[0]['id']
   end
 
 end
