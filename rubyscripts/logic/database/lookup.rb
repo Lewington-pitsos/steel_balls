f25bbdae00a7ebe6c9c98f32b1c55ce9f07d0c17
lewington <lewington@student.unimelb.edu.au>
2018-03-24 22:53:36 +1100

    lookup method abstraction

--- rubyscripts/logic/database/lookup.rb
+++ rubyscripts/logic/database/lookup.rb
@@ -8,6 +8,9 @@ class Lookup < Archivist
   @@optimal_tab = 'optimal_selections'
   @@resulting_tab = 'resulting_states'
 
+  @@selection_id_col = 'selection_id'
+  @@state_id_col = 'state_id'
+
   attr_accessor :tree
 
   def initialize(name=@@database_name)
@@ -20,9 +23,11 @@ class Lookup < Archivist
     @tree = build_state(first_id)
   end
 
+  private
+
   def build_state(state_id)
     state = get_by_id(state_id, @@state_tab)
-    selection_ids = optimal_selection_ids(state_id)
+    selection_ids = get_ids_by_id(state_id, optimal_selection_id, @@selection_id_col)
     selections = selection_ids.map do |selection_id|
       build_selection(selection_id)
     end
@@ -39,7 +44,7 @@ class Lookup < Archivist
   end
 
   def build_resulting_states(selection_id)
-    state_ids = get_resulting_states(selection_id)
+    state_ids = get_ids_by_id(selection_id, resulting_states, @@state_id_col)
     states = state_ids.map do |state_id|
       build_state(state_id)
     end
@@ -54,6 +59,18 @@ class Lookup < Archivist
     }
   end
 
+  def get_ids_by_id(id, callback, id_name)
+    # gets passed in a single id, a callback function (which should be mostly a query) and the column name of the ids we want to extract
+    # returns an array of ids
+    rows = callback(id)
+    ids = []
+    rows.each do |row|
+      ids << row[id_name]
+    end
+    ids
+  end
+
+
   # ================ Query Methods ================
 
   def optimal_selection_ids(state_id)
@@ -62,10 +79,10 @@ class Lookup < Archivist
         SELECT selection_id FROM #{@@optimal_tab}
         WHERE state_id = #{state_id};
       CMD
-    )
+    ).
   end
 
-  def get_resulting_states(selection_id)
+  def resulting_states(selection_id)
     @db.exec(
       <<~CMD
         SELECT state_id FROM #{@@resulting_tab}
@@ -87,7 +104,7 @@ class Lookup < Archivist
     @db.exec(
       <<~CMD
         SELECT * FROM scored_states
-        unknown = #{state[:unknown]} AND
+        WHERE unknown = #{state[:unknown]} AND
           possibly_lighter = #{state[:possibly_lighter]} AND
           possibly_heavier = #{state[:possibly_heavier]} AND
           normal = #{state[:normal]};
--- rubyscripts/testing/database/lookup_test.rb
+++ rubyscripts/testing/database/lookup_test.rb
@@ -4,7 +4,7 @@ require './rubyscripts/logic/state_manager'
 
 class ScoreCheckerTest < DatabaseTester
 
-  @@starting_state = {
+  @@starting_rated_state = {
     state: {
       unknown: 3,
       possibly_heavier: 0,
@@ -14,20 +14,62 @@ class ScoreCheckerTest < DatabaseTester
     rating: 0
   }
 
+  @@other_state = {
+    unknown: 1,
+    possibly_heavier: 0,
+    possibly_lighter: 0,
+    normal: 2
+  }
 
   def setup
     setup_database_for_testing
-    add_defaults(@@starting_state[:state])
+    add_defaults(@@starting_rated_state[:state])
     $WINNING_RATING = 12
     $DEFAULT_LENGTH = 3
-    @manager = StateManager.new(@@starting_state)
+    @manager = StateManager.new(@@starting_rated_state)
     @manager.score()
 
     @lookup = Lookup.new($DATABASE_NAME)
   end
 
   def test_retrival_by_id_works
+    assert_equal '1', @lookup.send(:get_by_id, '1', 'scored_states')['id']
+    assert_equal '2', @lookup.send(:get_by_id, '1', 'scored_states')['score']
+    assert_equal '1', @lookup.send(:get_by_id, '5', 'scored_states')['score']
+    assert_raises 'Error' do
+      @lookup.get_by_id('6', 'scored_states')
+    end
 
+    assert_equal '1', @lookup.send(:get_by_id, '1', 'selections')['left_id']
+    assert_equal '2', @lookup.send(:get_by_id, '1', 'selections')['right_id']
+    assert_equal '1', @lookup.send(:get_by_id, '4', 'selections')['left_id']
+    assert_equal '4', @lookup.send(:get_by_id, '4', 'selections')['right_id']
+    assert_raises 'Error' do
+      @lookup.get_by_id('8', 'selections')
+    end
+
+    assert_equal '0', @lookup.send(:get_by_id, '1', 'selection_sides')['unknown']
+    assert_equal '1', @lookup.send(:get_by_id, '2', 'selection_sides')['unknown']
+    assert_equal '1', @lookup.send(:get_by_id, '1', 'selection_sides')['normal']
+  end
+
+  def test_creates_proper_first_state
+    assert_equal $DEFAULT_LENGTH, @lookup.send(:first_state)[:unknown]
+
+    $DEFAULT_LENGTH = 9
+    assert_equal $DEFAULT_LENGTH, @lookup.send(:first_state)[:unknown]
+  end
+
+  def test_gets_correct_state_via_state_values
+    assert_equal '1', @lookup.send(:state_id_by_values, @@starting_rated_state[:state])
+    assert_equal '2', @lookup.send(:state_id_by_values, @@other_state)
+  end
+
+  def test_returns_all_optimal_selections
+    selections = @lookup.send(:optimal_selection_ids, '1')
+    selections.each do |sel|
+      assert_equal '7' sel['selection_id']
+    end
   end
 
   def teardown
