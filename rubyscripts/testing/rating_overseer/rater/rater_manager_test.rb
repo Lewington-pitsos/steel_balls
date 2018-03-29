bc401b078c50374791b4bb64cd474df72a42a985
lewington <lewington@student.unimelb.edu.au>
2018-03-29 13:22:02 +1100

    rater_manager passes all new (saving related) tests

--- rubyscripts/testing/rating_overseer/rater/rater_manager_test.rb
+++ rubyscripts/testing/rating_overseer/rater/rater_manager_test.rb
@@ -1,9 +1,22 @@
 require "minitest/autorun"
-require './rubyscripts/testing/test_defaults'
+require './rubyscripts/testing/database_tester'
+require './rubyscripts/logic/database/lookup'
 require './rubyscripts/logic/state_evaluator/rating_overseer/rater_manager'
 
 
-class RaterManagerTest < Minitest::Test
+class RaterManagerTest < DatabaseTester
+
+    @@placeholder_state = {
+      unknown: 8,
+      possibly_heavier: 0,
+      possibly_lighter: 0,
+      normal: 0
+    }
+
+    @@get_all_ratings = <<~CMD
+      SELECT rating FROM possible_selections;
+    CMD
+
 
     @@scored_and_kulled_selections = [{:rating=>29, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>0, :possibly_heavier=>2, :possibly_lighter=>0, :normal=>0}, :states=>[{:rating=>29, :state=>{:unknown=>1, :possibly_lighter=>2, :possibly_heavier=>0, :normal=>5}}, {:rating=>37, :state=>{:unknown=>0, :possibly_lighter=>0, :possibly_heavier=>1, :normal=>7}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>2, :normal=>5}}]}}, {:rating=>29, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>0, :possibly_heavier=>1, :possibly_lighter=>1, :normal=>0}, :states=>[{:rating=>29, :state=>{:unknown=>1, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>5}}, {:rating=>34, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>6}}]}}, {:rating=>29, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>0, :possibly_heavier=>0, :possibly_lighter=>2, :normal=>0}, :states=>[{:rating=>29, :state=>{:unknown=>1, :possibly_lighter=>0, :possibly_heavier=>2, :normal=>5}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>2, :possibly_heavier=>1, :normal=>5}}, {:rating=>37, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>0, :normal=>7}}]}}, {:rating=>31, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>1, :possibly_heavier=>1, :possibly_lighter=>0, :normal=>0}, :states=>[{:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>2, :normal=>5}}, {:rating=>34, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>6}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>2, :possibly_heavier=>1, :normal=>5}}]}}, {:rating=>31, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>1, :normal=>0}, :states=>[{:rating=>34, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>6}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>2, :possibly_heavier=>1, :normal=>5}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>2, :normal=>5}}]}}]
 
@@ -108,8 +121,10 @@ class RaterManagerTest < Minitest::Test
       @fancy_selection,
       @normal_selection
     ]
-
-    @manager = RaterManager.new(28)
+    setup_database_for_testing
+    add_defaults(@@placeholder_state)
+    @lookup = Lookup.new($DATABASE_NAME)
+    @manager = RaterManager.new(28, 1)
   end
 
   def test_correctly_weighs_and_scores
@@ -120,6 +135,23 @@ class RaterManagerTest < Minitest::Test
     assert_equal @@scored_and_kulled_selections.to_s, result.to_s
   end
 
+  def test_correctly_saves_rated_weighed_selections
+    @manager.weighed_and_scored(@example_selection_order)
+
+    @lookup.build_tree
+    assert_equal @@scored_and_kulled_selections.length, @lookup.tree['selections'].length
+
+    ratings = @db.exec(@@get_all_ratings)
+
+    @@scored_and_kulled_selections.each_with_index do |selection, index|
+      assert_equal selection[:selection][:states].length, @lookup.tree['selections'][index]['states'].length
+
+      assert_equal selection[:rating], ratings[index]['rating'].to_i
+    end
+
+  end
+
   def teardown
+    teardown_database
   end
 end
