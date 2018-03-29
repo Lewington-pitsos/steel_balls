58892077ce0ccbd3e9cb9f372c9b12541d67361a
lewington <lewington@student.unimelb.edu.au>
2018-03-29 12:52:22 +1100

    info_saver testing scaffold up

--- rubyscripts/logic/database/info_saver.rb
+++ rubyscripts/logic/database/info_saver.rb
@@ -7,19 +7,13 @@ require_relative './info_saver/selection_recorder'
 class InfoSaver
 
   def initialize()
-    @state = {}
-    @score = 0
-    @selections = []
-
-    @main_state_id = 0
-
     @state_recorder = StateRecorder.new()
     @selection_recorder = SelectionRecorder.new()
   end
 
   def save_everything(rated_selections, state_id)
     rated_selections.each do |selection|
-      @state_recorder.record_states(selection[:selection])
+      @state_recorder.record_states(selection)
       new_state_ids = @state_recorder.ids
       @selection_recorder.save_selection_data(selection, state_id, new_state_ids)
     end
@@ -34,7 +28,6 @@ class InfoSaver
 
   def close_everything
     @state_recorder.close()
-    @score_recorder.close()
     @selection_recorder.close()
   end
 end
--- rubyscripts/testing/database/info_saver_test.rb
+++ rubyscripts/testing/database/info_saver_test.rb
@@ -3,16 +3,25 @@ require './rubyscripts/logic/database/info_saver'
 
 class InfoSaverTest < DatabaseTester
 
+  @@placeholder_state = {
+    unknown: 4,
+    possibly_heavier: 0,
+    possibly_lighter: 1,
+    normal: 4
+  }
+
   @@rated_selections = [{:rating=>29, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>0, :possibly_heavier=>2, :possibly_lighter=>0, :normal=>0}, :states=>[{:rating=>29, :state=>{:unknown=>1, :possibly_lighter=>2, :possibly_heavier=>0, :normal=>5}}, {:rating=>37, :state=>{:unknown=>0, :possibly_lighter=>0, :possibly_heavier=>1, :normal=>7}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>2, :normal=>5}}]}}, {:rating=>29, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>0, :possibly_heavier=>1, :possibly_lighter=>1, :normal=>0}, :states=>[{:rating=>29, :state=>{:unknown=>1, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>5}}, {:rating=>34, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>6}}]}}, {:rating=>29, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>0, :possibly_heavier=>0, :possibly_lighter=>2, :normal=>0}, :states=>[{:rating=>29, :state=>{:unknown=>1, :possibly_lighter=>0, :possibly_heavier=>2, :normal=>5}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>2, :possibly_heavier=>1, :normal=>5}}, {:rating=>37, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>0, :normal=>7}}]}}, {:rating=>31, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>1, :possibly_heavier=>1, :possibly_lighter=>0, :normal=>0}, :states=>[{:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>2, :normal=>5}}, {:rating=>34, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>6}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>2, :possibly_heavier=>1, :normal=>5}}]}}, {:rating=>31, :selection=>{:left=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>0, :normal=>1}, :right=>{:unknown=>1, :possibly_heavier=>0, :possibly_lighter=>1, :normal=>0}, :states=>[{:rating=>34, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>1, :normal=>6}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>2, :possibly_heavier=>1, :normal=>5}}, {:rating=>31, :state=>{:unknown=>0, :possibly_lighter=>1, :possibly_heavier=>2, :normal=>5}}]}}]
 
 
   def setup
-    setup_database_for_testing
+    setup_database_for_testing()
+    add_defaults(@@placeholder_state)
     @saver = InfoSaver.new()
   end
 
   def test_saves_data_properly
-    @saver.save
+    @saver.save_everything(@@rated_selections, 1)
+
   end
 
   def teardown
