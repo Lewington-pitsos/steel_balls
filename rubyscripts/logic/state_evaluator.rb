1f399cfb1e7a46343cfab4ee72dcf96f7b931452
lewington <lewington@student.unimelb.edu.au>
2018-03-29 11:39:15 +1100

    state_evaluator scaffold up

--- rubyscripts/logic/state_evaluator.rb
+++ rubyscripts/logic/state_evaluator.rb
@@ -1,30 +1,37 @@
 # gets passed in  a state and it's rating
 
-# passes the ball state to the selection overseer, which returns a pre-weigh state (consisting of all possible selections given this state, plus all possible state arrangements)
-# passes the pre-weigh state plus the passed in state rating to the score overseer, which returns a score for the passed in state and an array of all the selections from the pre-weigh state with scores of their own
+# passes the ball state to the selection overseer, which saves and then returns all Rated Weighed Selections
+# passes the Rated Weighed Selections to the Scorer Manager which returns a score for each passed in Selections plus a new state score
 
 # passes the state score, the state and all the scored selections to a new state saver instance, which saves all of it.
 
 # finally, returns the state score to the state manager
 
-require_relative './state_evaluator/score_overseer'
-require_relative './state_evaluator/selection_overseer'
+require_relative './state_evaluator/scorer_manager'
+require_relative './state_evaluator/rating_overseer'
 require_relative './database/info_saver'
 
 class StateEvaluator
 
   def initialize(state, rating)
     @state = state
-    @selector = SelectionOverseer.new(@state)
-    @scorer = ScoreOverseer.new(rating)
+    @rater= RatingOverseer.new(@state, rating)
+    @scorer = ScorerManager.new()
     @recorder = InfoSaver.new()
+    @scored_state_info = []
   end
 
   def state_score
-    pre_weigh = @selector.selections_to_weigh
-    scored_state_info = @scorer.score(pre_weigh)
+    rated_weighed_selections = @rater.rated_weighed_selections
+    @scored_state_info = @scorer.scored_selections(rated_weighed_selections)
+    record_info
+    @scored_state_info[:state_score]
+  end
+
+  private
+
+  def record_info
     scored_state_info[:state] = @state
     @recorder.save_everything(scored_state_info)
-    scored_state_info[:state_score]
   end
 end
